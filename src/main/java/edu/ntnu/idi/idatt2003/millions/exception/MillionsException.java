7f6f8790d0526c4e6ae066bb5d0178366e635bcd
Real <53586056+RealEawo5@users.noreply.github.com>
2026-02-27 13:00:45 +0100

    base exception implementation

---
+++ src/main/java/edu/ntnu/idi/idatt2003/millions/exception/MillionsException.java
@@ -0,0 +1,16 @@
+package edu.ntnu.idi.idatt2003.millions.exception;
+
+/**
+ * Base exception for all Millions application errors.
+ */
+public class MillionsException extends Exception {
+
+    /**
+     * Constructs a new MillionsException with the given message.
+     *
+     * @param message the detail message
+     */
+    public MillionsException(String message) {
+        super(message);
+    }
+}
